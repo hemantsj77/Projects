c50a2a58106aad51382fb229ea295006918b88db
Hemant Singh Jandu <sardarhemantsingh2004@gmail.com>
2024-09-04 19:30:08 +0530

    Made some fixes

--- Weather-App-JavaScript/app.js
+++ Weather-App-JavaScript/app.js
@@ -12,7 +12,7 @@ weather.temperature = {
   unit: "celsius",
 };
 
-// APP CONSTS AND VARS
+// APP CONST AND VARS
 const KELVIN = 273;
 // API KEY
 const key = "82005d27a116c2880c8f0fcb866998a0";
@@ -73,7 +73,7 @@ function celsiusToFahrenheit(temperature) {
   return (temperature * 9) / 5 + 32;
 }
 
-// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENT
 tempElement.addEventListener("click", function () {
   if (weather.temperature.value === undefined) return;
 
